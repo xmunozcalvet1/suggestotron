87353aaff09cc5635d35b80623c00e2ed864d9ba
Xavi <xmunozcalvet1@gmail.com>
2018-09-09 13:21:22 +0200

    Downvote button added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 	  member do
 	    post 'upvote'
 	  end
+	  member do
+	  	post 'downvote'
+	  end
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
